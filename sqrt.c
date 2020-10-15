5d7c5961aae54e6a2f1e3806ffca197ac9f407d7
Anjali Deep <b18158@students.iitmandi.ac.in>
2020-10-15 20:32:37 +0530

    for conflicts

--- sqrt.c
+++ sqrt.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
 		return(0);
 	}
 	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	printf("Square root of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting\n");
 	return(0);
 
